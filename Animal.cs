cb56fd6eb841fee7e6335dd80f286fcfcad048c2
Alumgs <alumgs@majadamarcial.es>
2024-03-21 22:20:39 +0000

    Código completo

---
+++ Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAW.ETS._2__GIT3
+{
+    internal abstract class Animal
+    {
+        public string sexo = "";
+
+        public void AsignaSexo()
+        {
+            Random rng = new Random();
+            if (rng.Next(2) == 0)
+            {
+                sexo = "Hembra";
+            }
+            else
+            {
+                sexo = "Macho";
+            }
+        }
+    }
+}
---
+++ Mono.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAW.ETS._2__GIT3
+{
+    internal class Mono : Animal
+    {
+        int edad;
+        public Mono()
+        {
+            AsignaEdad();
+            AsignaSexo();
+        }
+
+        public void AsignaEdad()
+        {
+            Random rng = new Random();
+            rng.Next(30); // se descartan los 30 primeros números
+            edad = rng.Next(21);
+        }
+        public void Imprime()
+        {
+            Console.WriteLine($"Un mono {sexo} de {edad} años.");
+
+        }
+    }
+}
